a63b13350162f09882c1da50e3ba8b430adb855c
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2012-05-16 19:36:30 -0400

    Adding options to the rails generator

--- lib/highgroove_command.rb
+++ lib/highgroove_command.rb
@@ -4,7 +4,8 @@ class HighgrooveCommand < Thor
   include Thor::Actions
 
   desc "new NAME", "Generates a new project with the given name"
+  method_option :database, type: :string, default: 'postgresql', aliases: '-d'
   def new(name)
-    run "rails new #{name}"
+    run "rails new #{name} --skip-bundle -T -q -d #{options[:database]}"
   end
 end
