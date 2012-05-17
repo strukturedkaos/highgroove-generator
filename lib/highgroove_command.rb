0a28c23ffbd95b9fb3ceddaee0131ce9733543f0
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2012-05-16 20:31:03 -0400

    Setup rspec, capybara, and database_cleaner

--- lib/highgroove_command.rb
+++ lib/highgroove_command.rb
@@ -11,7 +11,39 @@ class HighgrooveCommand < Thor
       run "git init -q"
       gsub_file "Gemfile", /^ *#.*$/, ''
       gsub_file "Gemfile", /^ *$\n/, ''
+      append_to_file "Gemfile" do
+        <<-EOF
+          group :test, :development do
+            gem 'rspec-rails'
+          end
+          group :test do
+            gem 'capybara'
+            gem 'launchy'
+            gem 'database_cleaner'
+          end
+        EOF
+      end
       run "bundle install --quiet"
+      run "rails g rspec:install"
+      insert_into_file 'spec/spec_helper.rb', "\nrequire 'capybara/rspec'", after: "require 'rspec/autorun'"
+      gsub_file 'spec/spec_helper.rb', / *# Remove this[^\n]*\n *config\.fixture_path[^\n]*\n\n/m, ''
+      gsub_file 'spec/spec_helper.rb', /config.use_transactional_fixtures = true/, 'config.use_transactional_fixture = false'
+      insert_into_file 'spec/spec_helper.rb', after: "config.infer_base_class_for_anonymous_controllers = false\n" do
+        <<-EOF
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+        EOF
+      end
     end
   end
 end
